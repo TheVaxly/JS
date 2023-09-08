69335d5b1c750bdf48dfcc23ba75ca9a849c4849
Marten <marten.uiboupin@gmail.com>
2023-09-08 11:33:15 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+/*
+let name = 'john';
+console.log(name);
+const lastName = 'smith';
+console.log(lastName);
+*/
+
+/*
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+numbers.push(6);
+console.log(numbers);
+console.log(numbers[2]);
+*/
+
+const person = {
+    name: 'john',
+    lastName: 'smith',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA',
+    },
+    };
+
+console.log(person.hobbies[1]);
+person.email = 'marten.uiboupin@voco.ee';
+console.log(person);
