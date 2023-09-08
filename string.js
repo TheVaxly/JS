a956c4cc9b8786d8bcdf43c52df093669037111b
Marten <marten.uiboupin@gmail.com>
2023-09-08 13:22:20 +0300

    string 2

--- string.js
+++ string.js
@@ -3,4 +3,7 @@ const lastname = 'Uiboupin';
 
 let result = 'Hello there my name is ' + firstname + ' ' + lastname;
 
+result = lastname.substring(0, 4)
+result = lastname.slice(0, 4);
+result = lastname.slice(-3);
 console.log(result);
