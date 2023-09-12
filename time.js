cb18b380010a47c71a97548cf4e2b450355997eb
Marten <marten.uiboupin@gmail.com>
2023-09-12 11:21:17 +0300

    date

---
+++ time.js
@@ -0,0 +1,19 @@
+const today = new Date()
+let val = today;
+
+val = today.toString();
+val = today.toDateString();
+val = today.toTimeString();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMilliseconds();
+val = today.getMinutes();
+val = today.getTime();
+val = today.getTimezoneOffset();
+val = today.getMonth();
+val = today.getSeconds();
+
+
+console.log(val);
