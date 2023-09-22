90579d63576dd5b8f8e9ef2e7f87c9429162a045
Marten <marten.uiboupin@gmail.com>
2023-09-22 13:39:48 +0300

    localhost :0

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static('/home/marten/js-basics/isikukood website'));
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile('/home/marten/js-basics/isikukood website/isikukood.html');
     });
 
 app.listen(port, () => {
